3399756caaa73d27ced81abf4912405b85e88dcd
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-10-17 11:29:17 -0400

    Blog search feature initiation

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,16 +1,25 @@
 <div class="col-md-4">
+    
+<?php
+    
+    if (isset($_POST['submit'])) {
+        $search = $_POST['search'];
+    }
 
+?>
     <!-- Blog Search Well -->
     <div class="well">
     <h4>Blog Search</h4>
+    <form action="" method="post">
     <div class="input-group">
         <input type="text" class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
+            <button name= "submit" class="btn btn-default" type="submit">
                 <span class="glyphicon glyphicon-search"></span>
         </button>
         </span>
     </div>
+    </form>
     <!-- /.input-group -->
     </div>
 

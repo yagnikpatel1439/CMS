ae38294a52549c85a75342b3c728e96c9df1b7ce
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-10-24 22:01:52 -0400

    Admin add post

--- admin/include/add_post.php
+++ admin/include/add_post.php
@@ -1,5 +1,4 @@
 <?php
-
 if (isset($_POST['create_post'])) {
     $post_title = $_POST['title'];
     $post_author = $_POST['author'];
@@ -7,14 +6,22 @@ if (isset($_POST['create_post'])) {
     $post_status = $_POST['post_status'];
 
     $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['temp_name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
 
     $post_tags = $_POST['post_tags'];
     $post_content = $_POST['post_content'];
     $post_date = date('d-m-y');
     $post_comment_count = 4;
 
-    move_uploaded_file($post_image_temp, "../images/$post_image") ;
+    // echo $post_status;
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
+    $query .= "VALUES ({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', {$post_comment_count}, '{$post_status}' )";
+
+    $insert_post = mysqli_query($connection,$query);
+
+    confirm($insert_post);
 }
 
 ?>
@@ -56,7 +63,7 @@ if (isset($_POST['create_post'])) {
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="button" value="Publish Post" name="create_post"> 
+        <input class="btn btn-primary" type="submit" value="Publish Post" name="create_post"> 
     </div>
 
 </form>

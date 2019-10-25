278e5b0add313b4ebcddca66c884f19461d07852
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-10-25 17:27:42 -0400

    admin post connection with db

--- admin/include/edit_post.php
+++ admin/include/edit_post.php
@@ -1,23 +1,60 @@
+<?php
+
+    if (isset($_GET['p_id'])) {
+        $the_post_id = $_GET['p_id'];
+    }
+
+    $query = "SELECT * FROM posts";
+    $select_post_byId = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($select_post_byId)) {
+    $post_id = $row['post_id'];
+    $post_category_id = $row['post_category_id'];
+    $post_title = $row['post_title'];
+    $post_author = $row['post_author'];
+    $post_date = $row['post_date'];
+    $post_image = $row['post_image'];
+    $post_content = $row['post_content'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_status = $row['post_status'];
+
+    }
+
+?>
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="title">Title</label>
-        <input type="text" name="title" class="form-control">
+        <input value="<?php echo $post_title; ?>" type="text" name="title" class="form-control">
     </div>
 
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" name="post_category_id" class="form-control">
-    </div>
+        <select name="" id="post_category">
+            <?php
+                $query = "SELECT * FROM categories";
+                $select_categories= mysqli_query($connection, $query);
 
+                confirm($select_categories);
+
+                while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['id'];
+                $cat_title = $row['title'];
+                    
+                echo "<option value='$cat_id'>{$cat_title}</option>";
+                }
+            ?>
+        </select>
+    </div>
+                
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input type="text" name="author" class="form-control">
+        <input value="<?php echo $post_author; ?>" type="text" name="author" class="form-control">
     </div>
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" name="post_status" class="form-control">
+        <input value="<?php echo $post_status; ?>" type="text" name="post_status" class="form-control">
     </div>
 
     <div class="form-group">
@@ -27,12 +64,13 @@
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" name="post_tags" class="form-control">
+        <input value="<?php echo $post_tags; ?>" type="text" name="post_tags" class="form-control">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea name="post_content" class="form-control" id="" cols="30" rows="10"></textarea> 
+        <textarea name="post_content" class="form-control" id="" cols="30" rows="10"><?php echo $post_title; ?></textarea> 
+        
     </div>
 
     <div class="form-group">

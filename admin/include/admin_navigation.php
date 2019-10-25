c03696b9ba19dd2f8355590108eaf231e2673f5f
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-10-25 11:02:55 -0400

    Delete post

--- admin/include/admin_navigation.php
+++ admin/include/admin_navigation.php
@@ -122,7 +122,7 @@
                                 <a href="./posts.php">View Posts</a>
                             </li>
                             <li>
-                                <a href="include/add_post.php">Add Post</a>
+                                <a href="posts.php?source=add_post">Add Post</a>
                             </li>
                         </ul>
                     </li>

995b88dcd05ccbade4b53aab7476fa583d914c33
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-11-14 19:05:32 -0500

    dropdown introduced in add_post

--- admin/include/add_post.php
+++ admin/include/add_post.php
@@ -2,7 +2,7 @@
 if (isset($_POST['create_post'])) {
     $post_title = $_POST['title'];
     $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category_id'];
+    $post_category_id = $_POST['post_category'];
     $post_status = $_POST['post_status'];
 
     $post_image = $_FILES['image']['name'];
@@ -33,8 +33,21 @@ if (isset($_POST['create_post'])) {
     </div>
 
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" name="post_category_id" class="form-control">
+        <select name="post_category" id="">
+            <?php
+                $query = "SELECT * FROM categories";
+                $select_categories= mysqli_query($connection, $query);
+
+                confirm($select_categories);
+
+                while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['id'];
+                $cat_title = $row['title'];
+                    
+                echo "<option value='$cat_id'>{$cat_title}</option>";
+                }
+            ?>
+        </select>
     </div>
 
     <div class="form-group">

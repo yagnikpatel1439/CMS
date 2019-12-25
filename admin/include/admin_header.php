b3ec20a162f29796b125cf4d8b11f56b014e7a93
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-12-24 22:19:47 -0500

    user restriction implemented

--- admin/include/admin_header.php
+++ admin/include/admin_header.php
@@ -3,6 +3,16 @@
 <?php ob_start(); ?>
 <?php session_start(); ?>
 
+<?php 
+
+if (!isset($_SESSION['user_role'])) {
+    
+    header("Location: ../index.php");
+    
+}
+
+?>
+
 
 
 <!DOCTYPE html>
--- admin/include/admin_navigation.php
+++ admin/include/admin_navigation.php
@@ -104,7 +104,7 @@
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                            <a href="../include/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                         </li>
                     </ul>
                 </li>

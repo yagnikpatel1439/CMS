710d9811426085f4116560489f96540018c6c358
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-12-27 22:34:50 -0500

    Dashboard counts

--- admin/index.php
+++ admin/index.php
@@ -37,7 +37,15 @@
                                         <i class="fa fa-file-text fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                <div class='huge'>12</div>
+
+                                <!-- Counting All Posts -->
+                                    <?php 
+                                        $query = "SELECT * FROM posts";
+                                        $select_all_post = mysqli_query($connection, $query);
+                                        $post_count = mysqli_num_rows($select_all_post);
+                                        echo "<div class='huge'>{$post_count}</div>"
+                                    ?>
+
                                         <div>Posts</div>
                                     </div>
                                 </div>
@@ -59,7 +67,15 @@
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                    <div class='huge'>23</div>
+
+                                    <?php
+
+                                        $query = "SELECT * FROM comments";
+                                        $select_all_comments = mysqli_query($connection, $query);
+                                        $comment_count = mysqli_num_rows($select_all_comments);
+                                        echo "<div class='huge'>{$comment_count}</div>";
+                                    ?>
+                                    
                                     <div>Comments</div>
                                     </div>
                                 </div>
@@ -81,7 +97,15 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                    <div class='huge'>23</div>
+
+                                    <?php
+
+                                        $query = "SELECT * FROM users";
+                                        $select_all_users = mysqli_query($connection, $query);
+                                        $user_count = mysqli_num_rows($select_all_users);
+                                        echo "<div class='huge'>{$user_count}</div>";
+                                    ?>
+                                    
                                         <div> Users</div>
                                     </div>
                                 </div>
@@ -103,7 +127,15 @@
                                         <i class="fa fa-list fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>13</div>
+
+                                    <?php
+
+                                        $query = "SELECT * FROM categories";
+                                        $select_all_categories = mysqli_query($connection, $query);
+                                        $category_count = mysqli_num_rows($select_all_categories);
+                                        echo "<div class='huge'>{$category_count}</div>";
+                                    ?>
+                                    
                                         <div>Categories</div>
                                     </div>
                                 </div>

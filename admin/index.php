14586c40e7261457baf713bd94af159a53356a41
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2020-01-11 17:04:50 -0500

    Image linked to specific post

--- admin/index.php
+++ admin/index.php
@@ -153,6 +153,10 @@
                 <!-- /.row -->
                 <?php
 
+                $query = "SELECT * FROM posts WHERE post_status = 'published'";
+                $select_all_published_post = mysqli_query($connection, $query);
+                $post_published_count = mysqli_num_rows($select_all_published_post);  
+
                 $query = "SELECT * FROM posts WHERE post_status = 'draft'";
                 $select_all_draft_post = mysqli_query($connection, $query);
                 $post_draft_count = mysqli_num_rows($select_all_draft_post);  
@@ -178,10 +182,10 @@
 
             <?php
 
-            $element_text = ['Active Posts', 'Draft Post', 'Comments', 'Approved Comment','Users', 'Subscribers', 'Categories'];
-            $element_count = [$post_count,$post_draft_count, $comment_count, $comment_approved_count, $user_count, $subscriber_count, $category_count];
+            $element_text = ['All Posts','Active Posts', 'Draft Post', 'Comments', 'Approved Comment','Users', 'Subscribers', 'Categories'];
+            $element_count = [$post_count,$post_published_count, $post_draft_count, $comment_count, $comment_approved_count, $user_count, $subscriber_count, $category_count];
 
-            for ($i=0; $i < 7; $i++) { 
+            for ($i=0; $i < 8; $i++) { 
                 echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
             }
 
@@ -214,13 +218,6 @@
     </div>
     <!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <?php include "include/admin_footer.php" ?>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
     
-
-</body>
-
-</html>
